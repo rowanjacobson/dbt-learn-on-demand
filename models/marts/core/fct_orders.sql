0a5bb93d2a2c180538c8cbf78e43ea4bd91e6eee
sarah chasen <sarahchasen@sarahs-MacBook-Air-2.local>
2020-12-09 16:01:36 +0000

    updating files

---
+++ models/marts/core/fct_orders.sql
@@ -0,0 +1,29 @@
+
+with orders as (
+  select * from {{ ref('stg_orders') }}
+),
+
+payments as (
+  select * from {{ ref('stg_payments') }}
+),
+
+order_payments as (
+  select
+    order_id,
+    sum(case when status = 'success' then amount end) as amount
+  from payments
+  group by 1
+),
+
+final as (
+  SELECT
+    orders.order_id,
+    orders.customer_id,
+    orders.order_date,
+    coalesce(order_payments.amount,0) as amount
+  FROM orders
+  Left Join order_payments
+  on orders.order_id=order_payments.order_id
+)
+
+select * from final
---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,14 @@
+with payments as (
+    select
+      id as payment_id,
+      orderid as order_id,
+      paymentmethod as payment_mthod,
+      status,
+      amount/100 as amount,
+      created as created_at
+
+    from {{ source ('stripe','payment') }}
+
+)
+
+select * from payments
